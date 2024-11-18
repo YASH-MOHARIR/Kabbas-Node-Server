2c23dc05b26599368536b83f6eac83517fb964b3
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-11-18 11:56:11 +0530

    Added Kanbas Server

--- Lab5/index.js
+++ Lab5/index.js
@@ -2,6 +2,8 @@ import PathParameters from "./PathParameters.js";
 import QueryParameters from "./QueryParametes.js";
 import WorkingWithObjects from "./WOrkingWithObjects.js";
 import WorkingWithArrays from "./WorkingWithArrays.js";
+
+
 export default function Lab5(app) {
   app.get("/lab5/welcome", (req, res) => {
     res.send("Welcome to Lab 5");
