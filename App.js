435ea621b8693e0c7b9e6ad71bdde8a931687d68
YASH-MOHARIR <68592074+YASH-MOHARIR@users.noreply.github.com>
2024-11-19 12:07:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -15,16 +15,25 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000" || "http://localhost:3001",
-
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
-);
-
-const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
+ );
+ const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
+}
+app.use(session(sessionOptions));
+ 
+
   if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
@@ -32,12 +41,10 @@ const sessionOptions = {
       secure: true,
       domain: process.env.NODE_SERVER_DOMAIN,
     };
-  }
-app.use(session(sessionOptions));
+  } 
 
  
-
-app.use(session(sessionOptions));
+ 
 app.use(express.json());
 
 UserRoutes(app);
